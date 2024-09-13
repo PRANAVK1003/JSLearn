f7118f8ce08d9c89124fe64b7820196640ced873
PRANAVK <94790325+PRANAVK1003@users.noreply.github.com>
2024-09-13 20:32:45 +0530

    Add files via upload

---
+++ 01_basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version 
+//alert("Konnichiwa!!"). Doesn't run since we run the code in Nodejs , not browser 
+/*Data types 
+number => 2^53 
+bigint 
+string => ""
+boolean => true / false 
+null : Standalone value as well as type 
+undefined : Value not defined 
+Diff between  undefined and null is that null is a value while undefined describes that value is not deifned 
+symbol : Used to identify React Components . Design tools like Figma use it . 
+
+
+*/
+let age = 20 
+let a = null 
+
+console.log(typeof undefined ); //Output: Undefined 
+
